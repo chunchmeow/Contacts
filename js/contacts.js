36b451af317c96cbbacedf2c02a319ba2525e9bf
chunchmeow <chunchmeow@users.noreply.github.com>
2018-06-09 14:22:36 -0500

    Add files via upload

---
+++ js/contacts.js
@@ -0,0 +1,55 @@
+// contact list, usually would be a separate database
+  var contactList = [
+    {
+        id: 0, 
+        photo: 'http://ih.constantcontact.com/fs190/1101569576106/img/4717.jpg',
+        name: 'Keith Haring', 
+        email: 'kharing@animation.com', 
+        phone: '123-456-7890', 
+        url: 'www.haring.com/', 
+        notes: 'In addition to being impressed by the innovation and energy of his contemporaries, Haring was also inspired by the work of Jean Dubuffet, Pierre Alechinsky, William Burroughs, Brion Gysin and Robert Henri’s manifesto The Art Spirit, which asserted the fundamental independence of the artist. '
+    },
+      
+    {
+        id: 1, 
+        photo: 'http://anim.usc.edu/wp-content/uploads/2012/10/Peter-Chung.jpg', name: 'Peter Chung', email: 'pchung@aeonflux.com', phone: '123-456-7890', url: 'www.acmefilmworks.com/directors/peter-chung/', notes: 'He is best known for his unique style of animation, as the creator and director of Æon Flux, which ran as shorts on MTV\'s Liquid Television before launching as its own half-hour television series.'},
+      
+    {
+        id: 2, 
+        photo: 'https://burnersxxx.files.wordpress.com/2014/08/mike-judge-beavis.jpg', name: 'Mike Judge', email: 'mjudge@beavisandbutthead.com', phone: '123-456-7890', url: 'twitter.com/MikeJudge', notes: 'The Beavis & Butt-Head, King of the Hill, Office Space, Idiocracy, Silicon Valley guy.'},
+      
+    {
+        id: 3, 
+        photo: 'http://rawvision.com/sites/rawvision.com/files/Beyerlg.jpg', name: 'Mark Beyer', email: 'mbeyer@ThomasandNardo.com', phone: '123-456-7890', url: 'en.wikipedia.org/wiki/Mark_Beyer_%28comics%29', notes: 'An American cartoonist known for his bleak storylines, often featuring death, disfigurement, depression, and humiliation, which contrast with his childlike, geometric drawing style. Most of his stories are about the adventures of a codependent yet resentful couple named Amy and Jordan.'},
+      
+    {
+        id: 4, 
+        photo: 'http://www.animationmagazine.net/images/articles/eric_fogel.gif', name: 'Eric Fogel', email: 'eFogel@theHead.com', phone: '123-456-7890', url: 'www.tv.com/shows/the-head/', notes: 'Created by Eric Fogel, The Head is the story of Jim, a trade-school student in New York City who awakens one morning to find that his cranium has enlarged to mammoth proportions. A week later, out bursts Roy, a little purple alien with an odd sense of humor who has taken up residence in Jim\'s head.'},
+      
+      {
+        id: 5, 
+        photo: 'http://sacramentopress.com/wp-content/uploads/2013/01/close-up.jpg', 
+          name: 'Sam Kieth',
+          email: 'skeith@theMaxx.com',
+          phone: '123-456-7890', 
+          url: 'samkieth.blogspot.com/', 
+          notes: 'In 1993, Kieth left Marvel to create the original series The Maxx for fledgling publisher Image Comics. The Maxx ran 35 issues, all of which were plotted and illustrated by Kieth. William Messner-Loebs scripted #1-15 and Alan Moore wrote #21.[3] In 1995, The Maxx was adapted as part of MTV\'s short-lived animation series MTV\'s Oddities, which included Eric Fogel\'s The Head.'
+      },
+      
+    {
+        id: 6, 
+        photo: 'http://ak-hdl.buzzfed.com/static/2014-10/29/18/enhanced/webdr08/enhanced-20714-1414622484-2.jpg', name: 'Daria Morgendorffer', email: 'dmorgendorffer@lawndaleHS.com', phone: '123-456-7890', url: 'en.wikipedia.org/wiki/Daria', notes: 'Daria is an American animated television series created by Glenn Eichler and Susie Lewis Lynn for MTV. The series focuses on Daria Morgendorffer, a smart, acerbic, and somewhat misanthropic teenage girl who observes the world around her. The show is set in the fictional suburban American town of Lawndale and is a satire of high school life, and full of allusions to and criticisms of popular culture and social classes.'},
+      
+    {
+        id: 7, 
+        photo: 'http://41.media.tumblr.com/tumblr_m29v3nuOFQ1rtsg8ro1_r1_1280.png', name: 'Quinn Morgendorffer', email: 'quinnmorgendorffer@lawndaleHS.com', phone: '123-456-7890', url: 'en.wikipedia.org/wiki/List_of_Daria_characters', notes: 'Daria\'s materialistic and vain younger sister and school diva.[2] She is a member of Lawndale High School\'s Fashion Club. Quinn\'s only interests are fashion and dating, and she holds a Vice Presidency in Lawndale High\'s self-appointed Fashion Club.'},
+      
+    {
+        id: 8, 
+        photo: 'http://www.dariawiki.org/wiki/images/7/7a/Jane.gif', name: 'Jane Lane', email: 'janelane@lawndaleHS.com', phone: '123-456-7890', url: 'en.wikipedia.org/wiki/List_of_Daria_characters', notes: 'Daria\'s artistic best friend and fellow outcast, as well as the youngest of the five Lane siblings. While sharing some of Daria\'s misanthropic views and attitudes, Jane is much more lively and well adjusted than her best friend.'},
+      
+    {
+        id: 9, 
+        photo: 'http://i.imgur.com/2NL7mCm.gif', name: 'Trent Lane', email: 'tlane@MystikSpiral.com', phone: '123-456-7890', url: 'en.wikipedia.org/wiki/List_of_Daria_characters', notes: 'He wears spiky hair and plays in a band called Mystik Spiral.[3] A laid-back slacker, designed to be the stereotypical Generation X slacker of the 1990s, he\'s not sure whether or not he graduated from high school, and he drives the \'Trentmobile\', a rickety 1973 Plymouth Satellite coupe.'},
+      
+  ];
